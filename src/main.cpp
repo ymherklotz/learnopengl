0576f0e183d0e9c2a144391026233045b0179b3e
Yann Herklotz <git@yannherklotz.com>
2019-06-29 16:25:48 +0100

    Move into functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
+// Custom shader class to compile and manage what shaders are active
 #include "shader.h"
+// Image loading
 #include "stb_image.h"
 
+// OpenGL loader
 #include <glad/glad.h>
 
+// Header files for window management
 #include <GLFW/glfw3.h>
 
+// Matrix math libraries
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// IO and string C++ library
 #include <iostream>
 #include <string>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+// Functions that get called when an event happens
+void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
+// Helper functions
+GLFWwindow *createWindow();
+void setupGPUMemory();
+
+void initTextures();
+unsigned makeTexture(GLenum type, GLenum colour, std::string name);
+
+// Cube vertices that should be used to draw the cube.
 float vertices[] = {
     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
     0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
@@ -40,6 +55,7 @@ float vertices[] = {
     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
     -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
+// Positions at which to draw the cubes
 glm::vec3 cubePositions[] = {
     glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
     glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
@@ -47,6 +63,7 @@ glm::vec3 cubePositions[] = {
     glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
     glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
 
+// What vertices to use when drawing a face of the cube
 unsigned int indices[] = {
     // note that we start from 0!
     0, 1, 3, // first triangle
@@ -55,29 +72,125 @@ unsigned int indices[] = {
 
 int main()
 {
+    GLFWwindow *window = createWindow();
+    if (window == NULL)
+        return -1;
+
+    setupGPUMemory();
+
+    Shader shader("res/shaders/simple.vert", "res/shaders/simple.frag");
+    shader.use();
+
+    initTextures();
+
+    // Put the texture on the GPU
+    makeTexture(GL_TEXTURE0, GL_RGB, "res/img/container.jpg");
+
+    // Assign texture to the texture1 variable
+    shader.setUniform("texture1", 0);
+
+    // Flip the texture because of different coordinates and load smiley face
+    stbi_set_flip_vertically_on_load(true);
+    makeTexture(GL_TEXTURE1, GL_RGBA, "res/img/awesomeface.png");
+
+    // Assign the smiley texture to texture2
+    shader.setUniform("texture2", 1);
+
+    // Miscellaneous GL option
+    glEnable(GL_DEPTH_TEST);
+
+    // Initialise variables
+    float timeValue;
+    float greenValue;
+
+    // Main rendering loop
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // Create the view matrix that we will use to look at the scene
+        glm::mat4 view(1.f);
+        // note that we're translating the scene in the reverse direction of
+        // where we want to move
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::rotate(view, glm::radians(10.f * (float)glfwGetTime()), glm::vec3(1.f, 1.f, 1.f));
+
+        // Create the projection matrix that will turn 3D objects into 2D objects
+        glm::mat4 projection;
+        projection =
+            glm::perspective(glm::radians(45.f), 600.f / 800.f, 0.1f, 100.0f);
+
+        // Assign the matrices to view and projection variables
+        shader.setUniform("view", view);
+        shader.setUniform("projection", projection);
+
+        // Set the clear colour for the background
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Loop through all the cubes in the cubePositions list
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model(1.f);
+            shader.setUniform("model", model);
+
+            // Draw the 36 triangles for the 
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+GLFWwindow *createWindow()
+{
+    // Initialise GLFW for window management
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // Create a window
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        return NULL;
     }
     glfwMakeContextCurrent(window);
 
+    // Load OpenGL
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        return NULL;
     }
 
+    // Create a viewport where OpenGL can render to
     glViewport(0, 0, 800, 600);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // Set which functions to call when the window is resized
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+    return window;
+}
+
+// Complicated memory management on the GPU side
+void setupGPUMemory()
+{
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -102,10 +215,11 @@ int main()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                           (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+}
 
-    Shader shader("res/shaders/simple.vert", "res/shaders/simple.frag");
-    shader.use();
-
+// Initialise the use of textures
+void initTextures()
+{
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     float borderColor[] = {1.0f, 1.0f, 0.0f, 1.0f};
@@ -117,91 +231,25 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                     GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
 
+unsigned makeTexture(GLenum type, GLenum colour, std::string name)
+{
     int width, height, nrChannels;
     unsigned char *data =
-        stbi_load("res/img/container.jpg", &width, &height, &nrChannels, 0);
-
-    unsigned int texture;
+        stbi_load(name.c_str(), &width, &height, &nrChannels, 0);
+    
+    unsigned texture;
     glGenTextures(1, &texture);
-
-    glActiveTexture(GL_TEXTURE0);
+    glActiveTexture(type);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+    glTexImage2D(GL_TEXTURE_2D, 0, colour, width, height, 0, colour,
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
+    // Free the image
     stbi_image_free(data);
 
-    shader.setUniform("texture1", 0);
-
-    stbi_set_flip_vertically_on_load(true);
-    data =
-        stbi_load("res/img/awesomeface.png", &width, &height, &nrChannels, 0);
-
-    glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-                 GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    stbi_image_free(data);
-
-    shader.setUniform("texture2", 1);
-
-    glEnable(GL_DEPTH_TEST);
-
-    float timeValue;
-    float greenValue;
-
-    while (!glfwWindowShouldClose(window)) {
-        processInput(window);
-
-        glm::mat4 view(1.f);
-        // note that we're translating the scene in the reverse direction of
-        // where we want to move
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        view = glm::rotate(view, glm::radians(10.f * (float)glfwGetTime()), glm::vec3(1.f, 1.f, 1.f));
-
-        glm::mat4 projection;
-        projection =
-            glm::perspective(glm::radians(45.f), 600.f / 800.f, 0.1f, 100.0f);
-
-        shader.setUniform("view", view);
-        shader.setUniform("projection", projection);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        for (unsigned int i = 0; i < 10; i++) {
-            glm::mat4 model(1.f);
-            model       = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i * glfwGetTime();
-            model       = glm::rotate(model, glm::radians(angle),
-                                glm::vec3(1.0f, 0.3f, 0.5f));
-            shader.setUniform("model", model);
-
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+    return texture;
 }
